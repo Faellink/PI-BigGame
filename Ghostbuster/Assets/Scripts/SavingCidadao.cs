02fb2d773cd3c59c2511ec473cfe907cf6492a5b
Rafael Araujo <rafael0910i@gmail.com>
2020-04-24 18:52:09 -0300

    Sprint 01
    contador de pessoas

--- Ghostbuster/Assets/Scripts/SavingCidadao.cs
+++ Ghostbuster/Assets/Scripts/SavingCidadao.cs
@@ -7,6 +7,18 @@ public class SavingCidadao : MonoBehaviour
 
     public Transform ombro;
 
+    public PlayerController playerController;
+
+    public bool cidadaoNoOmbro;
+
+
+    void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +28,40 @@ public class SavingCidadao : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (playerController.carregarCidadao == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<CapsuleCollider>().enabled = false;
+                GetComponent<Rigidbody>().useGravity = false;
+                GetComponent<Rigidbody>().freezeRotation = true;
+                this.transform.position = ombro.position;
+                this.transform.parent = GameObject.Find("Ombro").transform;
+                Carregado();
+                cidadaoNoOmbro = true;
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                this.transform.parent = null;
+                GetComponent<Rigidbody>().freezeRotation = false;
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<CapsuleCollider>().enabled = true;
+                Jogado();
+                cidadaoNoOmbro = false;
+            }
+
+        }
+
+        if (playerController.carregarCidadao == false)
+        {
+            cidadaoNoOmbro = false;
+        }
+
     }
 
-     void OnMouseDown()
+    /*void OnMouseDown()
     {
         GetComponent<CapsuleCollider>().enabled = false;
         GetComponent<Rigidbody>().useGravity = false;
@@ -27,16 +69,16 @@ public class SavingCidadao : MonoBehaviour
         this.transform.position = ombro.position;
         this.transform.parent = GameObject.Find("Ombro").transform;
         Carregado();
-    }
+    }*/
 
-    void OnMouseUp()
+    /*void OnMouseUp()
     {
         this.transform.parent = null;
         GetComponent<Rigidbody>().freezeRotation = false;
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<CapsuleCollider>().enabled = true;
         Jogado();
-    }
+    }*/
 
     void Carregado()
     {
@@ -49,4 +91,5 @@ public class SavingCidadao : MonoBehaviour
         //this.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
         this.transform.up = ombro.up;
     }
+
 }
---
+++ Ghostbuster/Assets/Scripts/SavingCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SavingCounter : MonoBehaviour
+{
+    Text savingCounter;
+
+    public int savingNumber = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        savingCounter = GameObject.Find("SavingCounter").GetComponent<Text>();
+        savingNumber = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        savingCounter.text = "Pesoas Salvas: " + savingNumber;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cidadao"))
+        {
+            savingNumber++;
+            Debug.Log("Salvou");
+        }
+    }
+}
