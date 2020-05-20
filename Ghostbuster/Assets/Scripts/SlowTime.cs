ee73d13bad95250a6840b191239c1c2b40875849
Rafael <rafael0910i@gmail.com>
2020-05-20 17:09:35 -0300

    bullet time

---
+++ Ghostbuster/Assets/Scripts/SlowTime.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTime : MonoBehaviour
+{
+
+    public float bulletTimeMultiplier = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TestSlowTimeInput())
+        {
+            Time.timeScale = bulletTimeMultiplier;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    bool TestSlowTimeInput()
+    {
+        return Input.GetKey(KeyCode.LeftShift);
+    }
+}
