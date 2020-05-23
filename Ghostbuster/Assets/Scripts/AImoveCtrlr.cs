fa13f47fce8ed3d6fef98e4360af1d8079bdbf71
Fenzl <leongamedev@outlook.com>
2020-05-23 11:23:58 -0300

    CommitLeon2305
    Level e AI

---
+++ Ghostbuster/Assets/Scripts/AImoveCtrlr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AImoveCtrlr : MonoBehaviour
+{
+    public GameObject targetObject;
+    NavMeshAgent agentAI;
+
+    public int aiNumber;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentAI = GetComponent<NavMeshAgent>();
+        aiNumber = 0;
+    }
+
+    void SetAiActions()
+    {
+        if(aiNumber == 1)
+        {
+            targetObject = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agentAI.SetDestination(targetObject.transform.position);
+    }
+}
---
+++ Ghostbuster/Assets/Scripts/NewDestination.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewDestination : MonoBehaviour
+{
+    public int posX;
+    public int posZ;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Cidadao"))
+        {
+            posX = Random.Range(100,400);
+            posZ = Random.Range(100,400);
+            this.gameObject.transform.position = new Vector3 (posX,transform.position.y,posZ);
+        }
+    }
+}
