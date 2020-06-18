38bbb65a1f58c7884ef759597489bcb583198107
Rafael <rafael0910i@gmail.com>
2020-06-17 22:41:41 -0300

    main menu

---
+++ Ghostbuster/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    Renderer button;
+
+    public Material materialOn, materialOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        button.material = materialOn;
+    }
+
+    private void OnMouseExit()
+    {
+        button.material = materialOff;
+    }
+}
