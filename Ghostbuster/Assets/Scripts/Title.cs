936b401bf513ee299dd1498bb5538c08b9e4c1f4
Rafael <rafael0910i@gmail.com>
2020-06-22 23:51:17 -0300

    main menu mais bonito

--- Ghostbuster/Assets/Scripts/Title.cs
+++ Ghostbuster/Assets/Scripts/Title.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
 
+    public Scene level;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,18 @@ public class Title : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void EncimaButton()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            Debug.Log("achoe");
+        }
+    }
+
+    void PlayGame()
+    {
+
     }
 }
