2c95b997fb09ba1257c419eba15f8ec8e3059eaf
Rafael <rafael0910i@gmail.com>
2020-06-24 23:09:07 -0300

    menu pause

---
+++ Ghostbuster/Assets/Scripts/IconeCidadao.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconeCidadao : MonoBehaviour
+{
+
+    public Image cidadaoIcone;
+    public Transform target;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float minX = cidadaoIcone.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+
+        float minY = cidadaoIcone.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.height - minY;
+
+        Vector2 pos = Camera.main.WorldToScreenPoint(target.position);
+
+        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+        {
+            if (pos.x < Screen.width / 2)
+            {
+                pos.x = maxX;
+            }
+            else
+            {
+                pos.x = minX;
+            }
+        }
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        cidadaoIcone.transform.position = pos;
+    }
+}
---
+++ Ghostbuster/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public bool isPaused;
+    public GameObject pauseMenuCanvas;
+    public GameObject gameHud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == true)
+            {
+                isPaused = false;
+                Time.timeScale = 1;
+                Cursor.visible = false;
+                pauseMenuCanvas.SetActive(false);
+                gameHud.SetActive(true);
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                isPaused = true;
+                Cursor.visible = true;
+                Time.timeScale = 0;
+                gameHud.SetActive(false);
+                Cursor.lockState = CursorLockMode.None;
+                pauseMenuCanvas.SetActive(true);
+            }
+        }
+        
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1.0f;
+        gameHud.SetActive(true);
+        pauseMenuCanvas.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
